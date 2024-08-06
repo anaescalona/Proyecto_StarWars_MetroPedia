fe940bbb1d8ac1c9445d5f3276cc3fc8234b80b1
Julio <jflorespieve@gmail.com>
2024-08-06 11:07:11 -0400

    modifacion mision

--- Mision.py
+++ Mision.py
@@ -1,3 +1,10 @@
 class Mision:
-    def __init__(self, name, planet, spacecraft, weapons, ):
-        pass
+    def __init__(self, name, planet, spacecraft, weapons, personas):
+        self.m_name=name
+        self.planet=planet
+        self.spacecraft=spacecraft
+        self.weapons=weapons
+        self.personas=personas
+
+    def modify(self):
+        k
