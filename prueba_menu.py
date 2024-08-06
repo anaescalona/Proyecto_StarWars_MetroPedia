e0d3af614e3f42ce2896475225cccd6ecb0a2ec9
Julio <jflorespieve@gmail.com>
2024-08-06 10:56:35 -0400

    creacion de clase Mision

---
+++ prueba_menu.py
@@ -0,0 +1,17 @@
+def main():
+    option=[]
+    for i in range (1,6):
+        print(f'Bienvenido, Escoja su equipo tactico para la mision...')
+        option.append(input("""Nombre de la mision:
+              ---> """))
+       # input("""Planeta de destino:
+            #  ---> """)
+       #input("""Nave a utilizar:
+            #  ---> """)
+       # input("""Arma a utilizar:
+           #   ---> """)
+       # input("""Integrantes de la mision:
+          #    ---> """)
+       # if 
+        print(option)
+main()  
