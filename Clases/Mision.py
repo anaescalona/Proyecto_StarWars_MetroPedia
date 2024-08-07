7de177546a885f95d746f990d678c4896501f887
Julio <jflorespieve@gmail.com>
2024-08-06 22:44:23 -0400

    modificacion de menu

--- Clases/Mision.py
+++ Clases/Mision.py
@@ -8,7 +8,29 @@ class Mision:
 
     def modify(self):
         self.m_name=input()
-        self.planet=planet
-        self.spacecraft=spacecraft
-        self.weapons=weapons
-        self.personas=personas
+        self.planet=input()
+        self.spacecraft=input()
+        self.weapons=input()
+        self.personas=input()
+    
+    def construir(self):
+        
+        print(f'Bienvenido, Escoja su equipo tactico para la mision...')
+
+  
+        while True:
+            m_name=input("""Nombre de la mision:
+        ---> """)
+            if m_name.count > 0:
+                break
+            else:
+                print('El nombre de la misión debe contener por lo menos un carácter...')
+
+        input("""Planeta de destino:
+        ---> """)
+        input("""Nave a utilizar:
+        ---> """)
+        input("""Arma a utilizar:
+        ---> """)
+        input("""Integrantes de la mision:
+        ---> """)
