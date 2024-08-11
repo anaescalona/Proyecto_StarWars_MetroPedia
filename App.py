b56bdd22670fe3521e2a95a103e071cf891a9b31
Jean <jeanpieroguarino@gmail.com>
2024-08-11 19:01:59 -0400

    Documentacion menu principal

--- App.py
+++ App.py
@@ -1,14 +1,24 @@
 from Mision import menu_misiones
 from funciones_menuABCD import carga_api, Match_Menu_Parte1
-import os 
 from menu_EFG import menu_fgh
 
 class App:
 
-    def __init__(self) -> None:
-         '''self.peliculas, self.seres_vivos, self.personajes, self.planetas, self.lista_peliculas_saga, self.lista_seres_vivos, self.lista_personajes, self.lista_planetas, self.lista_vehiculos, self.lista_startships = carga_api()'''
+    def __init__(self):
+        '''
+        Inicializa la clase `App` y carga los datos desde la API.
+
+        Llama a la función `carga_api` para obtener los datos necesarios para el funcionamiento de la aplicación.
+        '''
+
+        self.peliculas, self.seres_vivos, self.personajes, self.planetas, self.lista_peliculas_saga, self.lista_seres_vivos, self.lista_personajes, self.lista_planetas, self.lista_vehiculos, self.lista_startships = carga_api()
 
     def menu_principal(self):
+        """
+        Muestra el menú principal y gestiona la navegación entre diferentes opciones.
+
+        Dependiendo de la opción seleccionada, se invoca la función correspondiente.
+        """
         while True: 
             print('''
               
