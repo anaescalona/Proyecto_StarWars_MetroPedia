9574561026d4e7942fb4278421ea0a9429c80f4e
anaescalona <ana.escalona@correo.unimet.edu.ve>
2024-08-11 21:11:26 -0400

    Actualizacion con Documentacion

--- Clases/Films.py
+++ Clases/Films.py
@@ -1,5 +1,23 @@
 class Films:
     def __init__(self,characters, planets,starships,vehicles,species,created,edited,producer,title,episode_id,director,release_date,opening_crawl,url):
+        """Inicializa la clase Films. Es el constructor de la clase. 
+
+        Args:
+            characters (str): atributo de la clase films 
+            planets (str): atributo de la clase films
+            starships (str): atributo de la clase films
+            vehicles (str): atributo de la clase films
+            species (str): atributo de la clase films
+            created (str): atributo de la clase films
+            edited (str): atributo de la clase films
+            producer (str): atributo de la clase films
+            title (str): atributo de la clase films
+            episode_id (str): atributo de la clase films
+            director (str): atributo de la clase films
+            release_date (str): atributo de la clase films
+            opening_crawl (str): atributo de la clase films
+            url (str): atributo de la clase films
+        """
         self.characters = characters
         self.planets = planets
         self.starships = starships
@@ -16,6 +34,8 @@ class Films:
         self.url = url
 
     def mostrar_pelicula(self):
+        """Permite mostrar en el formato indicado la información requerida
+        """
         print('')
         print(f'------------Titulo: {self.title}------------')
         print(f' \n-> Episodio: {self.episode_id} \n-> Fecha de Lanzamiento: {self.release_date} \n-> Nombre del Director:  {self.director} \n-> "Opening Crawl": {self.opening_crawl} ')
--- Clases/Movil.py
+++ Clases/Movil.py
@@ -1,5 +1,21 @@
 class Movil():
     def __init__(self, model,manufacturer,cost_in_credits,length,crew,passengers,max_atmosphering_speed,cargo_capacity,consumables,name,url,pilots = []):
+        """Inicializa la clase Movil. Es el constructor de la clase.
+
+            Args:
+                model (str): atributo de la clase Movil
+                manufacturer (str):atributo de la clase Movil
+                cost_in_credits (str): atributo de la clase Movil
+                length (str): atributo de la clase Movil
+                crew (str):atributo de la clase Movil
+                passengers (str): atributo de la clase Movil
+                max_atmosphering_speed (str): atributo de la clase Movil
+                cargo_capacity (str): atributo de la clase Movil
+                consumables (str): atributo de la clase Movil
+                name (str): atributo de la clase Movil
+                url (str):atributo de la clase Movil
+                pilots (list): atributo de la clase Movil
+            """
         self.model = model
         self.manufacturer = manufacturer
         self.cost_in_credits = cost_in_credits
@@ -17,6 +33,26 @@ class Movil():
 
 class Starships(Movil):
         def __init__(self, model, manufacturer, cost_in_credits, length, crew, passengers, max_atmosphering_speed, hyperdrive_rating, MGLT, cargo_capacity, consumables, name,url,starship_class,pilots=[]):
+          """Inicializa la clase Starships, tomando en cuenta que existe herencia donde la clase padre es Movil. Este el constructor de la clase al añadir los atributos de 
+          starships que no están presentes en la clase padre.
+
+                Args:
+                model (str): atributo de la clase Movil
+                manufacturer (str):atributo de la clase Movil
+                cost_in_credits (str): atributo de la clase Movil
+                length (str): atributo de la clase Movil
+                crew (str):atributo de la clase Movil
+                passengers (str): atributo de la clase Movil
+                max_atmosphering_speed (str): atributo de la clase Movil
+                hyperdrive_rating (str): atributo de la clase Starships(Movil)
+                MGLT (str): atributo de la clase Starships(Movil)
+                cargo_capacity (str): atributo de la clase Movil
+                consumables (str): atributo de la clase Movil
+                name (str): atributo de la clase Movil
+                url (str): atributo de la clase Movil
+                starship_class (str): atributo de la clase Movil
+                pilots (list): atributo de la clase Movil
+                """
           super().__init__(model, manufacturer, cost_in_credits, length, crew, passengers, max_atmosphering_speed, cargo_capacity, consumables, name, url,pilots)
           self.starships_class = starship_class
           self.hiperdrive_rating = hyperdrive_rating
@@ -24,6 +60,25 @@ class Starships(Movil):
 
 class Vehicles(Movil):
         def __init__(self, model, manufacturer, cost_in_credits, length, crew, passengers, max_atmosphering_speed, cargo_capacity, consumables,name, url,vehicle_class, films = [] ,pilots=[]):
+          """Inicializa la clase Vehicles, tomando en cuenta que existe herencia donde la clase padre es Movil. Este el constructor de la clase al añadir los atributos de 
+          Vehicles que no están presentes en la clase padre.
+
+                Args:
+                model (str): atributo de la clase Movil
+                manufacturer (str):atributo de la clase Movil
+                cost_in_credits (str): atributo de la clase Movil
+                length (str): atributo de la clase Movil
+                crew (str):atributo de la clase Movil
+                passengers (str): atributo de la clase Movil
+                max_atmosphering_speed (str): atributo de la clase Movil
+                cargo_capacity (str): atributo de la clase Movil
+                consumables (str): atributo de la clase Movil
+                name (str): atributo de la clase Movil
+                url (str):atributo de la clase Movil
+                vehicle_class (str): atributo de la clase Vehicles(Movil)
+                films (list): atributo de la clase Vehicles(Movil)
+                pilots (list, optional): atributo de la clase Movil
+                """
           super().__init__(model, manufacturer, cost_in_credits, length, crew, passengers, max_atmosphering_speed, cargo_capacity, consumables, name, url ,pilots)
           self.vehicles_class = vehicle_class
           self.films = films 
